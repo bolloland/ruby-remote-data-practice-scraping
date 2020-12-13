09d558ffe828da7cefc22360e5ae567e95acfbad
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-13 13:54:32 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,23 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".tout__label.heading.heading--level-4") 
+
+def get_courses
+courses.each do |course|
+    puts course.text.strip
+  end
+end
+.text
+.attributes
+.name
+etc to get the info we want
+
+
+  p doc.css(".tout__label.heading.heading--level-4")[0].name
+
+#html = open("https://flatironschool.com/")
